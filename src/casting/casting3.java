06cb1ff36c7e1ba40874209c0538098e1220149a
Jmboy713 <jimin713@naver.com>
2024-01-09 17:46:27 +0900

    casting

---
+++ src/casting/casting3.java
@@ -0,0 +1,12 @@
+package casting;
+
+public class casting3 {
+	public static void main(String[] args) {
+		long maxIntValue = 2147483647;
+		long maxIntOver = 2147483648L;
+		int value=0;
+		
+		System.out.println(value);
+
+	}
+}
