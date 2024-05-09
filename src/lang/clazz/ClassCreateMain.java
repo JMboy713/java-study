e4c5cb5f46408ee39e6666182e469c6e28e29237
Jmboy713 <jimin713@naver.com>
2024-05-09 15:31:13 +0900

    class

--- src/lang/clazz/ClassCreateMain.java
+++ src/lang/clazz/ClassCreateMain.java
@@ -1,2 +1,14 @@
-package lang.clazz;public class ClassCreateMain {
+package lang.clazz;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class ClassCreateMain {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+//        Class helloClass = Hello.class;
+        Class helloClass = Class.forName("lang.clazz.Hello");
+        Hello hello = (Hello) helloClass.getDeclaredConstructor().newInstance();
+        System.out.println(hello.hello());
+
+
+    }
 }
--- src/lang/clazz/ClassMetaMain.java
+++ src/lang/clazz/ClassMetaMain.java
@@ -1,2 +1,19 @@
-package lang.clazz;public class ClassMetaMain {
+package lang.clazz;
+
+import java.lang.reflect.Field;
+
+public class ClassMetaMain {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class clazz = String.class;// 1. 클래스에서 조회
+//        Class getClass = new String().getClass();// 2. 인스턴스에서 조회
+//        Class.forName("java.lang.String");// 3. 클래스 이름으로 조회
+
+        // 모든 필드 출력
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (String arg : args) {
+            
+        }
+
+    }
 }
