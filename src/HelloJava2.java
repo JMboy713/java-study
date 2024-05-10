0bcb189490c171e562c727d82d6ed989b0aec4bf
Jmboy713 <jimin713@naver.com>
2024-05-10 21:17:11 +0900

    test

--- src/HelloJava2.java
+++ src/HelloJava2.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class HelloJava2 {
 	public static void main(String[] args){
-		System.out.println("hello java2");
+
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		sc.nextLine();
+		for (int j = 0; j < i; j++) {
+			String s = sc.nextLine();
+			Stack<String> objects = new Stack<>();
+			char[] charArray = s.toCharArray();
+			String answer= "YES";
+			for (char c : charArray) {
+				if (c == '(') {
+					objects.push("(");
+				} else {
+					if (objects.isEmpty()) {
+						answer= "NO";
+						break;
+					} else {
+						objects.pop();
+					}
+				}
+			}
+			if (!objects.isEmpty()) {
+				answer= "NO";
+			}
+			System.out.println(answer);
+		}
+
+
 	}
 }
